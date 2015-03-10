93c9ab118ed0e255c00326039f6e5621aa947352
Weiwei SUN <ww.sun@outlook.com>
2015-03-10 20:18:27 +0800

    displaying list and orderBy filter

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,7 +1,13 @@
 angular.module('MainCtrl', []).controller('MainController', function() {
 
     var vm = this;
-
 	vm.tagline = 'To the moon and back!';
 
+    vm.posts = [
+        {title: 'post 1', upvotes: 5},
+        {title: 'post 2', upvotes: 2},
+        {title: 'post 3', upvotes: 15},
+        {title: 'post 4', upvotes: 9},
+        {title: 'post 5', upvotes: 4}
+    ];
 });
