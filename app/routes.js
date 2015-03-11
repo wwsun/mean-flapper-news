d70959d4030cec7691f6b0605d9058c537a2c2f5
Weiwei SUN <ww.sun@outlook.com>
2015-03-11 09:41:52 +0800

    GET /posts & POST /posts

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,7 @@
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
 module.exports = function(app) {
 
 	// server routes ===========================================================
@@ -6,8 +10,25 @@ module.exports = function(app) {
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
-	app.get('*', function(req, res) {
+	app.get('/', function(req, res) {
 		res.sendfile('./public/index.html');
 	});
 
+
+    app.route('/posts')
+
+        .get(function(req, res, next) {
+            Post.find(function(err, posts) {
+                if(err) return next(err);
+                res.json(posts);
+            })
+        })
+
+        .post(function(req, res, next) {
+            var post = new Post(req.body);
+            post.save(function(err, post) {
+                if(err) return next(err);
+                res.json(next);
+            })
+        });
 };
